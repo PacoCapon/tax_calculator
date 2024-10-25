5b1189a48499a3cd061ef13f6979d3f600df7d36
Paco Capón <pacocapon@MacBook-Pro-de-Paco.local>
2024-10-25 10:39:15 +0200

    Update readme

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Description:
 
 Implementation of a system that is capable of calculating taxes on the sales of products and services.
+You can start the server and CRUD over Products and Buyers, you can also calculate the taxes with the tax calculator that will create a register in the transaction table with the provided data.
 
 ## Versions:
 
@@ -15,3 +16,31 @@ Implementation of a system that is capable of calculating taxes on the sales of 
 * faker (https://github.com/faker-ruby) - Used to create fake data for tests
 * factory-bot-rails (https://github.com/thoughtbot/factory_bot_rails) - Used to create fake data for tests
 * byebug (https://github.com/deivid-rodriguez/byebug) - Used for debuggin purposes
+* country_select (https://github.com/countries/country_select) - Used for country select
+* bootstrap (https://rubygems.org/gems/bootstrap/versions/5.2.3) - Used for styling the views
+
+### Installation
+
+To install all gems:
+
+``` bundle install ```
+
+After that you have to run the migrations:
+
+``` rails db:migrate ```
+
+### Start server
+
+To start the server that will execute everyday at 8:00 UTC the task to create the disbursement:
+
+``` rails s ```
+
+### Fill the database with some mock data
+
+There is some information to test the implementation in the seeds file. To fill the development database with this data you have to use this:
+
+``` rails db:seed ```
+
+This will create some products, buyers and transactions.
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,44 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+require 'faker'
+
+# Clean database
+Transaction.destroy_all
+Product.destroy_all
+Buyer.destroy_all
+
+# Create some buyers
+5.times do
+  Buyer.create!(
+    name: Faker::Name.name,
+    buyer_type: ["individual", "company"].sample,
+    country: Faker::Address.country_code
+  )
+end
+
+# Create some products
+categories = ["food", "digital_service", "onsite_service"]
+
+10.times do
+  Product.create!(
+    name: Faker::Food.dish,
+    category: categories.sample
+  )
+end
+
+# Create some transactions
+buyers = Buyer.all
+products = Product.all
+
+10.times do
+  vat_value = rand(0..24) + rand.round(2)
+  Transaction.create!(
+    vat: vat_value,
+    country: buyers.sample.country,
+    transaction_type: ["good", "service", "digital"].sample,
+    calculation_date: Faker::Date.backward(days: 14),
+    buyer: buyers.sample,
+    product: products.sample
+  )
+end
+
+puts "#{Buyer.count} buyers, #{Product.count} products and #{Transaction.count} transactions were created."
